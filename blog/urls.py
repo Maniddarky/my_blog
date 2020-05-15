9dc17c1d6843d65a85ce4fa367d65ac1771915a5
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-05-15 16:01:39 +0530

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,40 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from my_app.views import index,blog,post,search, post_create, post_update,post_delete
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog, name='post-list'),
+    path('search/',search, name='search'),
+    path('create/', post_create, name='post-create'),
+    path('post/<id>/', post, name='post-details'),
+    path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<id>/delete/', post_delete, name='post-delete'),
+    path('tinymce/',include('tinymce.urls')),
+    path('accounts/', include('allauth.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
