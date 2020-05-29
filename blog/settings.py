325343b1e788b157e7baa717d062f82a3c05d592
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-05-29 16:25:10 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -86,25 +86,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-"""if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:"""
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'blogdb',
-            'USER': 'Manikandan',
-            'PASSWORD': 'Manikandan@7',
-            'HOST': 'teracoders.herokuapp.com',
-            'PORT': '',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
